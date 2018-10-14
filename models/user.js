88fdfdc54f0be2d31de7230080640005cc3b0a21
Egon Fiedler <efiedler93@gmail.com>
2018-10-14 14:30:28 -0700

    updated model for user still incomplete

--- models/user.js
+++ models/user.js
@@ -4,9 +4,18 @@ const Schema = mongoose.Schema;
 
 const User = mongoose.model('User', {
     name: String,
-    interests: String,
-    opinionId: {type: Schema.Types.ObjectId, ref: 'Opinion'}
+    password: String,
+    email: String
+    // interests: String,
+    //opinionId: {type: Schema.Types.ObjectId, ref: 'Opinion'}
     // charityId: {type: Schema.Types.ObjectId, ref: 'Charity'}
 });
 
 // module.exports = User;
+
+//async
+// const bcrypt = require('bcrypt');
+// const saltRounds = 10;
+// const myPlaintextPassword = 's0/\/\P4$$w0rD';
+// const someOtherPlaintextPassword = 'not_bacon';
+

69b17f54ace32a00534119b00317d951bbeca04c
Egon Fiedler <efiedler93@gmail.com>
2018-10-16 23:24:52 -0700

    update user model

--- models/user.js
+++ models/user.js
@@ -1,16 +1,43 @@
 
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
-const User = mongoose.model('User', {
-    name: String,
-    password: String,
-    email: String
-    // interests: String,
-    //opinionId: {type: Schema.Types.ObjectId, ref: 'Opinion'}
-    // charityId: {type: Schema.Types.ObjectId, ref: 'Charity'}
+// const User = mongoose.model('User', {
+//     name: String,
+//     password: String,
+//     email: String
+//     // interests: String,
+//     //opinionId: {type: Schema.Types.ObjectId, ref: 'Opinion'}
+//     // charityId: {type: Schema.Types.ObjectId, ref: 'Charity'}
+// });
+
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  passwordConf: {
+    type: String,
+    required: true,
+  }
 });
 
+var User = mongoose.model('User', UserSchema);
+
+module.exports = User;
 // module.exports = User;
 
 //async
